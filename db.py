80d20fb192bed6e47af55a2e522faedbc7e48f69
MelissaLemaire <18471707-MelissaLemaire@users.noreply.replit.com>
2023-07-02 16:04:39 +0000

    Verbinden met webservice, database en data weergeven.

---
+++ db.py
@@ -0,0 +1,49 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def init_db(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file, check_same_thread=False)
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS info
+            (
+                datetime TEXT,
+                sensor TEXT,
+                value REAL
+            )
+        """
+        )
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def add_data_point(conn, datetime, sensor, value):
+    sql = """
+            INSERT INTO info(datetime, sensor, value)
+            VALUES(?,?,?)
+            """
+    cur = conn.cursor()
+    cur.execute(sql, [datetime, sensor, value])
+    conn.commit()
+    return cur.lastrowid
+
+
+def list_data_points(conn):
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT datetime, sensor, value
+        FROM info ORDER BY datetime
+    """
+    )
+
+    rows = cur.fetchall()
+    return [
+        {"created": datetime.replace("T", " ") + ":00", "name": sensor, "value": value}
+        for datetime, sensor, value in rows
+    ]
--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-from flask import Flask
+import requests
+from flask import Flask, render_template, request, redirect
+
+import db
 
 app = Flask(__name__)
 
+TEMPERATURE_SERVICE = "https://po-meten.arhc.informatica.gsf.nl/4-logs-api.php"
 
-@app.route('/')
+db_conn = db.init_db("mel.db")
+
+
+@app.route("/")
 def index():
-    return 'Hallo ik ben Melissa!'
+    # krijg de data van de webservice
+    response_api = requests.post(
+        TEMPERATURE_SERVICE,
+        data={"req": "getall"},
+    )
+    api_data = response_api.json()["more"]
+
+    # krijg de data van mijn database
+    db_data = db.list_data_points(db_conn)
+
+    # samenvoegen van de data
+    merged_data = api_data + db_data
+
+    # data sorteren
+    sorted_data = sorted(merged_data, key=lambda d: d["created"])
+
+    # gebruik de template om data weer te geven
+    return render_template("index.html", data=sorted_data)
 
 
-app.run(host='0.0.0.0', port=81)
+@app.route("/data", methods=["POST"])
+def add_data_point():
+    datetime = request.form["datetime"]
+    sensor = request.form["sensor"]
+    value = request.form["value"]
+    db.add_data_point(db_conn, datetime, sensor, value)
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=81, debug=True)
